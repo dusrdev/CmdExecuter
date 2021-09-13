71c62a8c60715389bb6e8a62f02ea1a3b2b7240f
David Shnayder - Desktop <dusrdev@outlook.com>
2021-09-13 21:32:40 +0300

    Add project files.

---
+++ CmdExecuter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CmdExecuter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
