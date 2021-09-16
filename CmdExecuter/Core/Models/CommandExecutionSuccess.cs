8f3f9dd49ef1b056ff16066ed180d5f24fe9a4cd
David Shnayder <dusrdev@gmail.com>
2021-09-16 13:36:28 +0300

    Added more detailed execution result for success to allow logging

---
+++ CmdExecuter/Core/Models/CommandExecutionSuccess.cs
@@ -0,0 +1,11 @@
+﻿namespace CmdExecuter.Core.Models {
+    internal class CommandExecutionSuccess {
+        public string Command { get; init; }
+        public string Message {  get; init; }
+
+        public CommandExecutionSuccess(string command, string message) {
+            Command = command;
+            Message = message;
+        }
+    }
+}
