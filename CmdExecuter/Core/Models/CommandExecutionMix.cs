a58ec436c368f7fd6c117609dfa0345bb411f01f
David Shnayder <dusrdev@gmail.com>
2021-09-21 14:45:13 +0300

    Added support for new output model in which part of the command is successful and another fails

---
+++ CmdExecuter/Core/Models/CommandExecutionMix.cs
@@ -0,0 +1,13 @@
+﻿namespace CmdExecuter.Core.Models {
+    internal class CommandExecutionMix {
+        public string Command { get; init; }
+        public string SuccessOutput { get; init; }
+        public string ErrorOutput { get; init; }
+
+        public CommandExecutionMix(string command, string successOutput, string errorOutput) {
+            Command = command;
+            SuccessOutput = successOutput;
+            ErrorOutput = errorOutput;
+        }
+    }
+}
--- CmdExecuter/Core/Models/FileExecutionOutput.cs
+++ CmdExecuter/Core/Models/FileExecutionOutput.cs
@@ -6,14 +6,14 @@ namespace CmdExecuter.Core.Models {
     internal class FileExecutionOutput {
         public string FileName { get; init; }
 
-        public List<OneOf<CommandExecutionSuccess, CommandExecutionError>> Results { get; private set; }
+        public List<OneOf<CommandExecutionSuccess, CommandExecutionError, CommandExecutionMix>> Results { get; private set; }
 
         public FileExecutionOutput(string fileName) {
             FileName = fileName;
             Results = new();
         }
 
-        public void AddResult(OneOf<CommandExecutionSuccess, CommandExecutionError> result) {
+        public void AddResult(OneOf<CommandExecutionSuccess, CommandExecutionError, CommandExecutionMix> result) {
             Results.Add(result);
         }
     }
