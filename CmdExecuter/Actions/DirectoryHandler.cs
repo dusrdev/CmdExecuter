abee0b852609c9c9c1ddfe02f93ee9d920ef2188
David Shnayder <dusrdev@gmail.com>
2021-09-23 13:45:44 +0300

    For better organization moved command files into a resources folder and coded all involved IO operations and UI

---
+++ CmdExecuter/Actions/DirectoryHandler.cs
@@ -0,0 +1,44 @@
+﻿using CmdExecuter.Core.Components;
+
+using Spectre.Console;
+
+namespace CmdExecuter.Actions {
+    internal class DirectoryHandler {
+        private DirectoryCreator Creator { get; init; }
+
+        public DirectoryHandler() {
+            Creator = new();
+        }
+
+        public string GetResourcesPath() {
+            return Creator.ResourcesDirectoryPath;
+        }
+
+        public bool IsReady() {
+            if (!Creator.DoesResourcesDirectoryExist()) {
+                AnsiConsole.Markup("[white]Resources directory wasn't found, creating directory [darkslategray1]-->[/] [/]");
+                Creator.CreateResourcesDirectory().Switch(
+                    success => {
+                        AnsiConsole.MarkupLine("[springgreen1]Success[/]");
+                        AnsiConsole.MarkupLine("");
+                        AnsiConsole.MarkupLine("[white]Now add command files and re-launch the application.[/]");
+                    },
+                    error => {
+                        AnsiConsole.MarkupLine("[#990000]Fail[/]");
+                        AnsiConsole.MarkupLine("");
+                        AnsiConsole.MarkupLine("[white]Please create a directory inside the current directory and call it [springgreen1]Resources[/], and add your command files inside, then relaunch the application.[/]");
+                    });
+                AnsiConsole.MarkupLine("");
+                return false;
+            }
+
+            if (Creator.IsResourcesEmpty()) {
+                AnsiConsole.MarkupLine("[white]Resources directory is [#990000]Empty[/], add command files and re-launch the application.[/]");
+                AnsiConsole.MarkupLine("");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ CmdExecuter/Core/Components/DirectoryCreator.cs
@@ -0,0 +1,42 @@
+﻿using CmdExecuter.Core.Models;
+
+using OneOf;
+
+using System;
+using System.IO;
+
+namespace CmdExecuter.Core.Components {
+    internal class DirectoryCreator {
+        public readonly string ResourcesDirectoryPath;
+
+        public DirectoryCreator() {
+            ResourcesDirectoryPath = $"{Directory.GetCurrentDirectory()}\\Resources";
+        }
+
+        /// <summary>
+        /// Checks whether the resources directory exists
+        /// </summary>
+        public bool DoesResourcesDirectoryExist() {
+            return Directory.Exists(ResourcesDirectoryPath);
+        }
+
+        /// <summary>
+        /// Checks if there any files in the resources directory
+        /// </summary>
+        public bool IsResourcesEmpty() {
+            return Directory.GetFiles(ResourcesDirectoryPath).Length == 0;
+        }
+
+        /// <summary>
+        /// Creates the resources directory if doesn't exist
+        /// </summary>
+        public OneOf<Success, Error> CreateResourcesDirectory() {
+            try {
+                Directory.CreateDirectory(ResourcesDirectoryPath);
+            } catch (Exception ex) {
+                return new Error(ex.Message);
+            }
+            return new Success("Directory created successfully.");
+        }
+    }
+}
